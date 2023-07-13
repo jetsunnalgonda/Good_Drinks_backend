f0b4ff05cfab64e901f9b68943b3af1a644982ca
Haluk Isik <isikhaluk@gmail.com>
2023-07-13 12:19:22 +0300

    wsgi

---
+++ wsgi.py
@@ -0,0 +1,22 @@
+
+HELLO_WORLD = """<html>
+<head>
+    <title>PythonAnywhere hosted web application</title>
+</head>
+<body>
+<h1>Good Drinks API</h1>
+<p>
+    Welcome to the Good Drinks API
+</p>
+<p>
+    To ask the developer questions about the API, visit <a href="https://jetsunnalgonda.github.io">their portfolio.</a>
+</p>
+</body>
+</html>"""
+
+# import sys
+# path = '/home/nalgonda/api'
+# if path not in sys.path:
+#     sys.path.insert(0, path)
+
+from application import app as application
